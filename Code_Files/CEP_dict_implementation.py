e6ebf82094c538041b934fd900bd2608493f4f76
Abdur Rahman <khalil.ahmad1547@gmail.com>
2021-01-11 18:02:18 +0500

    command line arguments are implemented
    Now symptoms are taken from csv file through command line and row number.

--- Code_Files/CEP_dict_implementation.py
+++ Code_Files/CEP_dict_implementation.py
@@ -1,8 +1,40 @@
 from baysianClasifier import BayesianClassifier
+import sys
+import pandas
 
 myClasifier = BayesianClassifier()
-myClasifier.getData("../DataSet/Training.csv", ["n/a", "na", "--"], ',')
+
+try:
+    myClasifier.getData("../DataSet/Training.csv", ["n/a", "na", "--"], ',')
+except:
+    print("file not found")
+    sys.exit()
+
+print("Cleaning data ...")
 myClasifier.cleanGivenData()
+
+print("training on given data ...")
 myClasifier.train([132], list(range(0, 132)))
-results = myClasifier.runTests([ "red_sore_around_nose"])
-print(myClasifier.predict(results))
+
+testingFile = sys.argv[1]
+
+testingSymptoms = sys.argv[2]
+print(testingSymptoms)
+try:
+    testingdata = pandas.read_csv(testingFile)
+except:
+    print("Enter a correct file name")
+    sys.exit()
+
+# getting all the symptoms from the mentioned row
+userIn = int(testingSymptoms)
+index = 0
+testingSymptoms = []
+for i in list(testingdata.iloc[userIn, :]):
+    # if status is 1 add to list
+    if i == 1:
+        testingSymptoms.append(testingdata.columns[index])
+    index = index + 1
+
+# predicting for given symptoms
+print(myClasifier.predict(myClasifier.runTests(testingSymptoms)))
